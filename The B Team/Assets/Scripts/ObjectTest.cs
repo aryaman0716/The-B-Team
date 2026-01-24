aec46b9610514c90b302dc68955d18db8f3c02d3
Kananake143 <2502432@uad.ac.uk>
2026-01-24 20:41:02 +0000

    Show text in Console

--- The B Team/Assets/Scripts/ObjectTest.cs
+++ The B Team/Assets/Scripts/ObjectTest.cs
@@ -2,15 +2,21 @@ using UnityEngine;
 
 public class NewMonoBehaviourScript : MonoBehaviour
 {
+    void Awake()
+    {
+        Debug.Log("Hello Unity");
+    }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Debug.Log("Start Unity");
+
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.Rotate(0, 2,0);
+        Debug.Log("End Unity");
     }
 }
