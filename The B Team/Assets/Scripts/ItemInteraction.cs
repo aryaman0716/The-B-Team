f388df7636cb123cc0f5e4dc750cc87aae9e1e3f
Kananake143 <2502432@uad.ac.uk>
2026-01-28 22:58:20 +0000

    change button and hide item

--- The B Team/Assets/Scripts/ItemInteraction.cs
+++ The B Team/Assets/Scripts/ItemInteraction.cs
@@ -1,80 +1,129 @@
 using UnityEngine;
+using UnityEngine.UI; // Image UI
+using System.Collections.Generic;
 
 public class ItemInteraction : MonoBehaviour
 {
     [Header("Settings")]
     [SerializeField] private float interactDistance = 4f;
-    [SerializeField] private KeyCode interactKey = KeyCode.E;
+    [SerializeField] private int maxInventorySize = 5;
 
     [Header("References")]
     [SerializeField] private Transform propsHolder;
     [SerializeField] private Camera playerCamera;
+    [SerializeField] private GameObject inventoryPanel; //  InventoryPanel
+    [SerializeField] private Image[] inventorySlots; //  Image Slot 5 
+
+    [SerializeField] private List<GameObject> inventory = new List<GameObject>();
+    private GameObject currentHoldingItem;
 
     void Start()
     {
-        
+        // hide UI when start game
+        inventoryPanel.SetActive(false);
+
         if (propsHolder.childCount > 0)
         {
-            SetupItemInHand(propsHolder.GetChild(0).gameObject);
+            currentHoldingItem = propsHolder.GetChild(0).gameObject;
+            SetupItemInHand(currentHoldingItem);
         }
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(interactKey))
+        // 1. Left Click = 0
+        if (Input.GetMouseButtonDown(0))
         {
-            TryInteract();
+            TryPickUpItem();
+        }
+
+        // 2. Hold Right Click = 1
+        if (currentHoldingItem != null)
+        {
+            bool isHolding = Input.GetMouseButton(1);
+            currentHoldingItem.SetActive(isHolding);
+            inventoryPanel.SetActive(isHolding); // UI
         }
     }
 
-    void TryInteract()
+    void TryPickUpItem()
     {
         RaycastHit hit;
         if (Physics.Raycast(playerCamera.transform.position, playerCamera.transform.forward, out hit, interactDistance))
         {
             if (hit.collider.CompareTag("Interactable"))
             {
-                SwapItems(hit.collider.gameObject);
+                HandleInventoryLogic(hit.collider.gameObject);
             }
         }
     }
 
-    void SwapItems(GameObject newItem)
+    void HandleInventoryLogic(GameObject newItem)
     {
-        
-        if (propsHolder.childCount > 0)
+        if (inventory.Count >= maxInventorySize)
         {
-            GameObject oldItem = propsHolder.GetChild(0).gameObject;
-            oldItem.transform.SetParent(null);
+            DropCurrentItem();
+        }
+        AddToInventory(newItem);
+    }
 
-            Rigidbody oldRb = oldItem.GetComponent<Rigidbody>();
-            if (oldRb != null)
+    void AddToInventory(GameObject item)
+    {
+        inventory.Add(item);
+        UpdateInventoryUI(); // update image in UI
+        SetupItemInHand(item);
+        item.SetActive(false);
+        currentHoldingItem = item;
+    }
+
+    void UpdateInventoryUI()
+    {
+        // clear image
+        foreach (Image slot in inventorySlots) { slot.sprite = null; slot.enabled = false; }
+
+        // input image in List
+        for (int i = 0; i < inventory.Count; i++)
+        {
+            
+            Sprite itemSprite = inventory[i].GetComponent<Image>()?.sprite;
+            if (itemSprite != null)
             {
-                oldRb.isKinematic = false; 
-                oldRb.useGravity = true;
+                inventorySlots[i].sprite = itemSprite;
+                inventorySlots[i].enabled = true;
+            }
+        }
+    }
+
+    void DropCurrentItem()
+    {
+        if (currentHoldingItem != null)
+        {
+            inventory.Remove(currentHoldingItem);
+            currentHoldingItem.transform.SetParent(null);
+
+            Rigidbody rb = currentHoldingItem.GetComponent<Rigidbody>();
+            if (rb != null)
+            {
+                rb.isKinematic = false;
+                rb.useGravity = true;
             }
 
-            
-            oldItem.transform.position = transform.position + transform.forward * 1.2f + Vector3.up * 0.5f;
+            currentHoldingItem.transform.position = transform.position + transform.forward * 1.5f;
+            currentHoldingItem.SetActive(true);
+            currentHoldingItem = null;
+            UpdateInventoryUI();
         }
-
-        
-        SetupItemInHand(newItem);
     }
 
     void SetupItemInHand(GameObject item)
     {
         item.transform.SetParent(propsHolder);
-
-       
         Rigidbody rb = item.GetComponent<Rigidbody>();
         if (rb != null)
         {
             rb.isKinematic = true;
             rb.useGravity = false;
         }
-
-        
         item.transform.localPosition = new Vector3(0, 0, 0.5f);
         item.transform.localRotation = Quaternion.identity;
     }
