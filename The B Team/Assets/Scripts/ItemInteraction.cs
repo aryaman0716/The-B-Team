a1544c87b6b1b127c029dfdf16ffca79a52b88c5
Kananake143 <2502432@uad.ac.uk>
2026-01-31 02:08:47 +0000

    Revert "pick up and drop item"
    This reverts commit 9271aa82784b3a37b35c93b2825c74162625c31c.

--- The B Team/Assets/Scripts/ItemInteraction.cs
+++ The B Team/Assets/Scripts/ItemInteraction.cs
@@ -5,15 +5,13 @@ public class ItemInteraction : MonoBehaviour
 {
     [Header("Settings")]
     [SerializeField] private float interactDistance = 4f;
-    //[SerializeField] private int maxInventorySize = 5;
-    [SerializeField] private float throwForce = 600f;
+    [SerializeField] private int maxInventorySize = 5;
 
     [Header("References")]
     [SerializeField] private Transform propsHolder;
     [SerializeField] private Camera playerCamera;
 
-
-    //private List<GameObject> inventory = new List<GameObject>();
+    private List<GameObject> inventory = new List<GameObject>();
     private GameObject currentHoldingItem;
 
     void Start()
@@ -23,31 +21,38 @@ public class ItemInteraction : MonoBehaviour
         {
             currentHoldingItem = propsHolder.GetChild(0).gameObject;
             SetupItemInHand(currentHoldingItem);
-            //currentHoldingItem.SetActive(false); // hide if click right ->show last item
+            currentHoldingItem.SetActive(false); // hide if click right ->show last item
         }
     }
 
     void Update()
     {
+        // 1. click left -> get item
         if (Input.GetMouseButtonDown(0))
         {
             TryPickUpItem();
         }
 
-        if (currentHoldingItem != null && Input.GetKeyDown(KeyCode.Q)) 
+        // 2. hold right -> show item
+        if (currentHoldingItem != null)
         {
-            DropCurrentItem();
+            bool isHolding = Input.GetMouseButton(1);
+
+            
+            if (currentHoldingItem.activeSelf != isHolding)
+            {
+                currentHoldingItem.SetActive(isHolding);
+            }
         }
     }
 
     void TryPickUpItem()
     {
-        // (Viewport Center)
-        Ray ray = playerCamera.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
         RaycastHit hit;
-
-        if (Physics.Raycast(ray, out hit, interactDistance))
+        // Raycast 
+        if (Physics.Raycast(playerCamera.transform.position, playerCamera.transform.forward, out hit, interactDistance))
         {
+            //  Tag obj
             if (hit.collider.CompareTag("Interactable"))
             {
                 HandleInventoryLogic(hit.collider.gameObject);
@@ -57,24 +62,17 @@ public class ItemInteraction : MonoBehaviour
 
     void HandleInventoryLogic(GameObject newItem)
     {
-        
-        if (currentHoldingItem != null)
+        // 5 items full
+        if (inventory.Count >= maxInventorySize)
         {
             DropCurrentItem();
         }
-
-        
-        SetupItemInHand(newItem);
-
-        
-        currentHoldingItem = newItem;
-
-        
+        AddToInventory(newItem);
     }
 
     void AddToInventory(GameObject item)
     {
-        //inventory.Add(item);
+        inventory.Add(item);
         SetupItemInHand(item);
         item.SetActive(false); // hide bag
         currentHoldingItem = item;
@@ -84,24 +82,19 @@ public class ItemInteraction : MonoBehaviour
     {
         if (currentHoldingItem != null)
         {
-            
-
-            
+            inventory.Remove(currentHoldingItem);
             currentHoldingItem.transform.SetParent(null);
 
             
-            currentHoldingItem.transform.position = playerCamera.transform.position + playerCamera.transform.forward * 1.0f;
-
             Rigidbody rb = currentHoldingItem.GetComponent<Rigidbody>();
             if (rb != null)
             {
                 rb.isKinematic = false;
                 rb.useGravity = true;
-
-               
-                rb.AddForce(playerCamera.transform.forward * 600f);
             }
 
+           
+            currentHoldingItem.transform.position = transform.position + transform.forward * 1.5f;
             currentHoldingItem.SetActive(true);
             currentHoldingItem = null;
         }
@@ -111,19 +104,16 @@ public class ItemInteraction : MonoBehaviour
     {
         item.transform.SetParent(propsHolder);
 
+        
         Rigidbody rb = item.GetComponent<Rigidbody>();
         if (rb != null)
         {
             rb.isKinematic = true;
             rb.useGravity = false;
-            
-            rb.linearVelocity = Vector3.zero; 
-            rb.angularVelocity = Vector3.zero;
         }
 
-        item.transform.localPosition = new Vector3(0.5f, -0.4f, 1.0f);
-        item.transform.localRotation = Quaternion.Euler(0, 90, 0);
+        
+        item.transform.localPosition = new Vector3(0, 0, 0.5f);
+        item.transform.localRotation = Quaternion.identity;
     }
-
-
 }
