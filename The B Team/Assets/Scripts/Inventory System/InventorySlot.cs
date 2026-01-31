bb51ad9139e50e33f9821d5bd5f0ae6f9d0bc884
Kananake143 <2502432@uad.ac.uk>
2026-01-31 01:58:22 +0000

    Revert "Incorporated scriptable objects for inventory management"
    This reverts commit 670b36af62f457ead8eafd77a467396f1238b33d.

--- The B Team/Assets/Scripts/Inventory System/InventorySlot.cs
+++
@@ -1,6 +0,0 @@
-[System.Serializable]
-public class InventorySlot
-{
-    public ItemData item;
-    public bool IsEmpty => item == null; // Check if the slot is empty
-}
--- The B Team/Assets/Scripts/Inventory System/WorldItem.cs
+++
@@ -1,6 +0,0 @@
-using UnityEngine;
-
-public class WorldItem : MonoBehaviour
-{
-    public ItemData itemData; // Reference to the ItemData ScriptableObject
-}
