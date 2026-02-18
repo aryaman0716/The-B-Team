abdd7ae74c40b78e9b3bdeea1b44790bd86f1c65
Kananake143 <2502432@uad.ac.uk>
2026-02-18 23:38:19 +0000

    projectlie tomato fixed bugs

--- The B Team/Assets/Scripts/TomatoTool.cs
+++ The B Team/Assets/Scripts/TomatoTool.cs
@@ -3,13 +3,17 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Tools/Tomato Tool")]
 public class TomatoTool : ToolData
 {
-    public GameObject projectilePrefab;
+    public GameObject projectilePrefab; 
     public float throwForce = 20f;
+
     public override void UseTool(Transform origin)
     {
-        //Debug.Log("Tomato thrown!");
-        if (projectilePrefab == null || origin == null) return;
-        GameObject tomato = Instantiate(projectilePrefab, origin.position + origin.forward, origin.rotation);
+        if (projectilePrefab == null) return;
+
+        
+        Vector3 spawnPos = origin.position + (origin.forward * 1.0f);
+
+        GameObject tomato = Instantiate(projectilePrefab, spawnPos, origin.rotation);
         Rigidbody rb = tomato.GetComponent<Rigidbody>();
 
         if (rb != null)
