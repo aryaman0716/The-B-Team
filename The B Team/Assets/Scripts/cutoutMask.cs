a1798bc7e730fc39b141e01c2ef2cf64f9a61585
DrAlb3rto <darogers88@gmail.com>
2026-02-20 00:43:14 +0000

    merge?

---
+++ The B Team/Assets/Scripts/cutoutMask.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Rendering;
+public class cutoutMask : Image
+{
+
+    public override Material materialForRendering
+    {
+        get {
+        
+            Material material = new Material(base.materialForRendering);
+            material.SetInt("_StencilComp", (int)CompareFunction.NotEqual);
+            return material;
+        }
+    }
+    
+
+}
