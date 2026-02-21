c9c4e2e427246b3f06fb2c31b020c9b99cfedba9
DrAlb3rto <darogers88@gmail.com>
2026-02-21 15:14:21 +0000

    Knife Implemented

--- The B Team/Assets/Scripts/KnifeTool.cs
+++ The B Team/Assets/Scripts/KnifeTool.cs
@@ -3,8 +3,12 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Tools/Knife Tool")]
 public class KnifeTool : ToolData
 {
+
+    public Animator animator;
     public override void UseTool(Transform origin)
     {
         Debug.Log("Knife used!");
+        animator = GameObject.FindGameObjectWithTag("Tool").GetComponent<Animator>();
+        animator.SetTrigger("Slash");
     }
 }
