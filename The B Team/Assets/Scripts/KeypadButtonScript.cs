d5c5261e97e0a8998d6507d5a25a5454f27e7828
DrAlb3rto <darogers88@gmail.com>
2026-02-22 10:35:17 +0000

    bugfix

--- The B Team/Assets/Scripts/KeypadButtonScript.cs
+++ The B Team/Assets/Scripts/KeypadButtonScript.cs
@@ -5,12 +5,16 @@ public class KeypadButtonScript : MonoBehaviour
 {
     public string buttonID;
     public KeyPadController controller;
-
+    private GameObject Player;
 
     //if you want the button to be for a keypad or a general use button
     public bool keyPad = true;
     [SerializeField] private UnityEvent buttonFunction;
 
+    private void Awake()
+    {
+        Player = GameObject.FindGameObjectWithTag("Player");
+    }
     public void PressButton()
     {
         if (keyPad)
@@ -27,6 +31,18 @@ public class KeypadButtonScript : MonoBehaviour
    
     void OnMouseDown()
     {
-        PressButton();
+
+        if (Player!=null)
+        {
+            if (PlayerDistance() < 3f) PressButton();
+        }
+        else PressButton();
+    }
+
+
+    private float PlayerDistance()
+    {
+        float distance = Vector3.Distance(Player.transform.position, transform.position);
+        return distance;
     }
 }
