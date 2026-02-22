76abbb697d5b51bc206879c6aef2afaa0e43b558
DrAlb3rto <darogers88@gmail.com>
2026-02-22 10:19:09 +0000

    New Function System for keypad and button and new button

--- The B Team/Assets/Scripts/KeypadButtonScript.cs
+++ The B Team/Assets/Scripts/KeypadButtonScript.cs
@@ -1,14 +1,27 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 public class KeypadButtonScript : MonoBehaviour
 {
     public string buttonID;
     public KeyPadController controller;
 
+
+    //if you want the button to be for a keypad or a general use button
+    public bool keyPad = true;
+    [SerializeField] private UnityEvent buttonFunction;
+
     public void PressButton()
     {
-        if (controller != null)
+        if (keyPad)
+        {
+            if (controller != null)
             controller.AddNumber(buttonID);
+        }
+        else
+        {
+            buttonFunction.Invoke();
+        }
     }
 
    
