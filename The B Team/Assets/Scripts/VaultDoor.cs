ddaba7922c5bb3867317cab479d57aae1235a286
wooples0 <joshmmckay4@gmail.com>
2026-03-28 10:47:00 +0000

    Room 3 + 4 mesh fixes
    No secret room in vault yet, still need to implement on vault south wall

--- The B Team/Assets/Scripts/VaultDoor.cs
+++ The B Team/Assets/Scripts/VaultDoor.cs
@@ -12,7 +12,7 @@ public class VaultDoor : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
