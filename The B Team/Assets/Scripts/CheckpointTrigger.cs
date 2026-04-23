00f4ade97b622a0f25e3f77794979981db194699
AryamanMukherjee <aryamanm03@gmail.com>
2026-04-23 11:51:00 +0100

    All checkpoints work now

--- The B Team/Assets/Scripts/CheckpointTrigger.cs
+++ The B Team/Assets/Scripts/CheckpointTrigger.cs
@@ -4,12 +4,19 @@ public class CheckpointTrigger : MonoBehaviour
     public int checkpointIndex; 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
-        { 
+        if (other.CompareTag("Player")) return;
+
+        int currentCheckpoint = PlayerPrefs.GetInt("CheckpointIndex", 0);
+        if (checkpointIndex > currentCheckpoint)
+        {
             // saving checkpoint using player prefs to keep it persistent across the game
             PlayerPrefs.SetInt("CheckpointIndex", checkpointIndex);
-            PlayerPrefs.Save(); 
+            PlayerPrefs.Save();
             Debug.Log("Checkpoint saved: " + checkpointIndex);
+        }
+        else
+        {
+            Debug.Log("Checkpoint passed or already reached: " + checkpointIndex);
         }
     }
 }
