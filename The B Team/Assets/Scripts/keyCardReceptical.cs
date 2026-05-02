76f5b6719244126efe00082ba195bee289c7f64f
wooples0 <joshmmckay4@gmail.com>
2026-05-02 10:35:40 +0100

    Keycard placement highlight + keycard reader screen changes colour when activated

--- The B Team/Assets/Scripts/keyCardReceptical.cs
+++ The B Team/Assets/Scripts/keyCardReceptical.cs
@@ -3,25 +3,38 @@ using UnityEngine;
 public class keyCardReceptical : MonoBehaviour
 {
     public KeypadButtonScript button, button2;
+    public PlacementEmitter keyCardEmitter;
+
+    public MeshRenderer screenMesh;
+    public Material[] materials;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        screenMesh.material = materials[0];
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-    }
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.gameObject.CompareTag("KeyCard"))
+        if (keyCardEmitter.gameObject.activeSelf && keyCardEmitter.IsPlaced)
         {
+            screenMesh.material = materials[1];
             button.needsKey = false;
             button2.needsKey = false;
-            collision.gameObject.SetActive(false);
+            keyCardEmitter.gameObject.SetActive(false);
+            ObjectiveManager.Instance.CompleteObjective("Find a keycard to unlock the staff elevator.", "Find a way to blast through the door");
         }
-        ObjectiveManager.Instance.CompleteObjective("Find a keycard to unlock the staff elevator.", "Find a way to blast through the vault door.");
+        
     }
+    //private void OnCollisionEnter(Collision collision)
+    //{
+    //    if (collision.gameObject.CompareTag("KeyCard"))
+    //    {
+    //        button.needsKey = false;
+    //        button2.needsKey = false;
+    //        collision.gameObject.SetActive(false);
+    //    }
+    //    ObjectiveManager.Instance.CompleteObjective("Find a keycard to unlock the staff elevator.", "Find a way to blast through the vault door.");
+    //}
 }
